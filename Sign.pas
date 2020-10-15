ba86b5782a871b2e086b35e478db506d4e0a6204
MirzaDgtu <mirzyali@mail.ru>
2020-10-15 15:36:35 +0700

    Design Sign form already

--- Sign.pas
+++ Sign.pas
@@ -4,10 +4,30 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.StdCtrls, System.ImageList, FMX.ImgList, FMX.Controls.Presentation,
+  FMX.Edit, FMX.ListBox, FMX.Objects;
 
 type
   TSignForm = class(TForm)
+    MainLayout: TLayout;
+    SignHeaderTB: TToolBar;
+    SignBottomTB: TToolBar;
+    Images: TImageList;
+    SettingParamSBtn: TSpeedButton;
+    SignHeadeLbl: TLabel;
+    SignSBtn: TSpeedButton;
+    DefaultSBtn: TSpeedButton;
+    AOuthRect: TRectangle;
+    AOuthLB: TListBox;
+    SignLBGH: TListBoxGroupHeader;
+    ServerLBI: TListBoxItem;
+    UserLBI: TListBoxItem;
+    ServerEdit: TEdit;
+    ClearServerEditBtn: TClearEditButton;
+    UserEdit: TEdit;
+    UserBtn: TButton;
+    ClearUserEditBtn: TClearEditButton;
   private
     { Private declarations }
   public
@@ -20,5 +40,7 @@ var
 implementation
 
 {$R *.fmx}
+{$R *.LgXhdpiPh.fmx ANDROID}
+{$R *.XLgXhdpiTb.fmx ANDROID}
 
 end.
